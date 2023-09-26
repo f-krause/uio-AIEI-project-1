8011d3efc232338891b0ea07c1644da7585737f5
Felix Krause <felix_krause1999@yahoo.de>
2023-09-26 20:45:59 +0200

    changes logical order of microgrid classes

--- src/microgrid.py
+++ src/microgrid.py
@@ -4,63 +4,6 @@ from gym import spaces
 from params import *
 
 
-class MicrogridEnv(gym.Env):
-    def __init__(self):
-        # Define the action and observation spaces
-        self.action_space = spaces.Discrete(3)  # Assuming 3 possible actions for each component
-        self.observation_space = spaces.Dict(
-            {
-                "solar": spaces.Box(0, np.inf, dtype=float),
-                "wind": spaces.Box(0, np.inf, dtype=float),
-                "electricity price": spaces.Box(0, np.inf, dtype=float),
-                "load": spaces.Box(0, np.inf, dtype=float),
-                "battery": spaces.Box(SOC_min, SOC_max, dtype=float),
-            }
-        )
-        # Initialize your Microgrid
-        self.microgrid = Microgrid()
-
-    def reset(self):
-        # Reset the Microgrid to its initial state
-        self.microgrid = Microgrid()
-        # Return the initial observation
-        return self.get_observation()
-
-    def step(self, action):
-        # Execute the chosen action on the Microgrid
-        # Update the Microgrid's state and compute the reward
-        # TODO: put actions
-        self.microgrid.transition()
-
-        # Calculate the reward based on your cost reduction goal
-        reward = self.compute_reward()
-
-        # Check if the episode is done (you can define a termination condition here)
-        done = False  # You need to define when an episode is done
-
-        # Return the next observation, reward, done flag, and any additional info
-        return self.get_observation(), reward, done, {}
-
-    def get_observation(self):
-        # Extract relevant information from the Microgrid's state and return it as an observation
-        observation = [
-            self.microgrid.workingstatus[0],
-            self.microgrid.workingstatus[1],
-            self.microgrid.workingstatus[2],
-            self.microgrid.SOC,
-            self.microgrid.solarirradiance,
-            self.microgrid.windspeed,
-            # Add other relevant variables here
-        ]
-        return np.array(observation)
-
-    def compute_reward(self):
-        return self.microgrid.EnergyConsumption() * sell_back_price + self.microgrid.OperationalCost() - self.microgrid.SoldBackReward()
-
-    def render(self, mode='human'):
-        pass
-
-
 class Microgrid(object):
     def __init__(self,
                  workingstatus=[0, 0, 0],  # solar PV, wind turbine, generator
@@ -172,3 +115,60 @@ class Microgrid(object):
         print("wind speed =", self.windspeed, file=file)
         print("Microgrid Energy Consumption =", self.energy_consumption(), file=file)
         print("Microgrid Operational Cost =", self.operational_cost)
+
+
+class MicrogridEnv(gym.Env):
+    def __init__(self):
+        # Define the action and observation spaces
+        self.action_space = spaces.Discrete(3)  # Assuming 3 possible actions for each component
+        self.observation_space = spaces.Dict(
+            {
+                "solar": spaces.Box(0, np.inf, dtype=float),
+                "wind": spaces.Box(0, np.inf, dtype=float),
+                "electricity price": spaces.Box(0, np.inf, dtype=float),
+                "load": spaces.Box(0, np.inf, dtype=float),
+                "battery": spaces.Box(SOC_min, SOC_max, dtype=float),
+            }
+        )
+        # Initialize your Microgrid
+        self.microgrid = Microgrid()
+
+    def reset(self):
+        # Reset the Microgrid to its initial state
+        self.microgrid = Microgrid()
+        # Return the initial observation
+        return self.get_observation()
+
+    def step(self, action):
+        # Execute the chosen action on the Microgrid
+        # Update the Microgrid's state and compute the reward
+        # TODO: put actions
+        self.microgrid.transition()
+
+        # Calculate the reward based on your cost reduction goal
+        reward = self.compute_reward()
+
+        # Check if the episode is done (you can define a termination condition here)
+        done = False  # You need to define when an episode is done
+
+        # Return the next observation, reward, done flag, and any additional info
+        return self.get_observation(), reward, done, {}
+
+    def get_observation(self):
+        # Extract relevant information from the Microgrid's state and return it as an observation
+        observation = [
+            self.microgrid.workingstatus[0],
+            self.microgrid.workingstatus[1],
+            self.microgrid.workingstatus[2],
+            self.microgrid.SOC,
+            self.microgrid.solarirradiance,
+            self.microgrid.windspeed,
+            # Add other relevant variables here
+        ]
+        return np.array(observation)
+
+    def compute_reward(self):
+        return self.microgrid.EnergyConsumption() * sell_back_price + self.microgrid.OperationalCost() - self.microgrid.SoldBackReward()
+
+    def render(self, mode='human'):
+        pass
